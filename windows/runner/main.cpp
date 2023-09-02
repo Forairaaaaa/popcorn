740b0b47d2d6334090acd6348e5fe58d31cf522a
Forairaaaaa <applesyqd@outlook.com>
2023-09-02 22:23:51 +0800

    create

---
+++ windows/runner/main.cpp
@@ -0,0 +1,49 @@
+#include <flutter/dart_project.h>
+#include <flutter/flutter_view_controller.h>
+#include <windows.h>
+
+#include "flutter_window.h"
+#include "utils.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
+                      _In_ wchar_t *command_line, _In_ int show_command) {
+  // Attach to console when present (e.g., 'flutter run') or create a
+  // new console when running with a debugger.
+  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+    CreateAndAttachConsole();
+  }
+
+  // Initialize COM, so that it is available for use in the library and/or
+  // plugins.
+  ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+
+  flutter::DartProject project(L"data");
+
+  std::vector<std::string> command_line_arguments =
+      GetCommandLineArguments();
+
+  project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
+
+  // Create window at the center 
+  int defaultWidth = 1280;
+  int defaultHeight = 840;
+  int xPos = (GetSystemMetrics(SM_CXSCREEN) - defaultWidth) / 2;
+  int yPos = (GetSystemMetrics(SM_CYSCREEN) - defaultHeight) / 2;
+    
+  FlutterWindow window(project);
+  Win32Window::Point origin(xPos, yPos);
+  Win32Window::Size size(defaultWidth, defaultHeight);
+  if (!window.Create(L"popcorn", origin, size)) {
+    return EXIT_FAILURE;
+  }
+  window.SetQuitOnClose(true);
+
+  ::MSG msg;
+  while (::GetMessage(&msg, nullptr, 0, 0)) {
+    ::TranslateMessage(&msg);
+    ::DispatchMessage(&msg);
+  }
+
+  ::CoUninitialize();
+  return EXIT_SUCCESS;
+}
