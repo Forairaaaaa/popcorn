7da4168c65e84b760f6556c9a1a29ae453c7b9df
Forairaaaaa <applesyqd@outlook.com>
2023-09-05 13:52:56 +0800

    :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'pages/home/popcorn_home_page.dart';
+
+import 'presenters/serial_port/serial_port_presenter.dart';
+
+
+
+
+
 
 void main() {
+
+
   runApp(const MyApp());
 }
 
@@ -19,10 +29,76 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
         useMaterial3: true,
       ),
-      initialRoute: '/home',
-      routes: {
-        '/home' :(context) => const PopcornHomePage(),
-      },
+      // initialRoute: '/home',
+      // routes: {
+      //   '/home' :(context) => const PopcornHomePage(),
+      // },
+      home: const ShitPage(),
+    );
+  }
+}
+
+
+
+
+class ShitPage extends StatefulWidget {
+  const ShitPage({super.key});
+
+  @override
+  State<ShitPage> createState() => _ShitPageState();
+}
+
+class _ShitPageState extends State<ShitPage> {
+
+
+  String textshit = '';
+
+  @override
+  void initState() {
+
+
+    for (var shit in SerialPortPresenter().availablePorts) {
+      print(shit);
+    }
+
+    SerialPortPresenter().open(SerialPortPresenter().availablePorts.last);
+
+    // SerialPortPresenter().rxStreamRaw.listen((event) {
+    //   setState(() {
+    //     textshit += ascii.decode(event);
+    //   });
+    // });
+
+    SerialPortPresenter().rxStream.listen((event) {
+      setState(() {
+        textshit += event;
+      });
+    });
+
+
+
+
+
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Container(
+        child: SelectableText(
+          textshit
+          ,
+          style: TextStyle(
+            fontFamily: 'CourierPrime',
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+      ),
     );
   }
 }
